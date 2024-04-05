a358a6ea921aa140cd7e530710651c6830e894ae
LeeJaeUng <lsam3333333@gmail.com>
2024-04-05 12:19:51 +0900

    Board에 Cell 생성 추가

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -6,33 +6,23 @@ using UnityEngine.InputSystem;
 public class Board : MonoBehaviour
 {
     /// 셀을 생성하고 관리 (리셋, 지뢰 설치 등등)
-    /// 입력에 따라 셀 변환
-    /// 게임 상태 변환
+    public GameObject cellPrefab;
+
+    int width = 16;
+    int height = 16;
+    int mineCount = 10;
 
     [SerializeField] Cell[] cells = null;
 
-    float x;
-    public float X
-    {
-        get => x; set => x = value;
-    }
-
-    float y;
-    public float Y
-    {
-        get => y; set => y = value;
-    }
-
-    public int width = 8;
-    public int height = 8;
+    const float Distance = 1.0f;
 
     PlayerInputActions inputAction;
 
     private void Awake()
     {
-        inputAction = new PlayerInputActions();
-        GameManager manager = GameManager.Instance;
-        manager.onGameReady += RefreshBoard;
+        //inputAction = new PlayerInputActions();
+        //GameManager manager = GameManager.Instance;
+        //manager.onGameReady += RefreshBoard;
         //manager.onGamePlay += ;
         //manager.onGameClear += ;
         //manager.onGameOver += ;
@@ -40,28 +30,48 @@ public class Board : MonoBehaviour
 
     #region InputActions
 
-    private void OnEnable()
-    {
-        inputAction.Player.Enable();
-    }
-    private void OnDisable()
-    {
-        inputAction.Player.Disable();
-    }
+    //private void OnEnable()
+    //{
+    //    inputAction.Player.Enable();
+    //}
+    //private void OnDisable()
+    //{
+    //    inputAction.Player.Disable();
+    //}
 
     #endregion
 
-    private void InitializedBoard()
+    public void Initialize(int newWidth, int newHeight, int newMineCount)
     {
+        width = newWidth;
+        height = newHeight;
+        mineCount = newMineCount;
 
+        cells = new Cell[width * height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject cellObj = Instantiate(cellPrefab, transform);
+                Cell cell = cellObj.GetComponent<Cell>();
+                cell.transform.localPosition = new Vector3(x * Distance, -y * Distance);
+
+                int id = x + y * width;
+                cells[id] = cell;
+                cellObj.name = $"Cell_{id}_({x},{y})";
+            }
+        }
     }
 
     void RefreshBoard()
     {
 
     }
-    
 
+    /// 입력에 따라 셀 변환
+
+    /// 게임 상태 변환
 
 
 }
