e8613fbcaeee83fd00ae2828f270d493cd89705e
LeeJaeUng <lsam3333333@gmail.com>
2024-04-08 11:39:14 +0900

    셔플과 마인 생성

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,11 +7,13 @@ using UnityEngine.InputSystem;
 
 public class Board : MonoBehaviour
 {
-    /// 셀을 생성하고 관리 (리셋, 지뢰 설치 등등)
+    /// <summary>
+    /// 생성할 셀의 프리팹
+    /// </summary>
     public GameObject cellPrefab;
 
     /// <summary>
-    /// 보드의 가로길이
+    /// 보드의 가로 길이
     /// </summary>
     int width = 16;
 
@@ -20,21 +23,24 @@ public class Board : MonoBehaviour
     int height = 16;
 
     /// <summary>
-    /// 배치될 지회의 개수
+    /// 배치될 지뢰의 개수
     /// </summary>
     int mineCount = 10;
 
     /// <summary>
     /// 이 보드가 생성한 모든 셀
     /// </summary>
-    Cell[] cells = null;
+    Cell[] cells;
 
     /// <summary>
     /// 셀 한 변의 크기
     /// </summary>
     const float Distance = 1.0f;
 
-    PlayerInputActions inputAction;
+    /// <summary>
+    /// 인풋시스템을 위한 인풋액션
+    /// </summary>
+    PlayerInputActions inputActions;
 
     public Sprite[] openCellImage;
     public Sprite this[OpenCellType type] => openCellImage[(int)type];
@@ -43,105 +49,116 @@ public class Board : MonoBehaviour
 
     private void Awake()
     {
-        inputAction = new PlayerInputActions();
-        //GameManager manager = GameManager.Instance;
-        //manager.onGameReady += RefreshBoard;
-        //manager.onGamePlay += ;
-        //manager.onGameClear += ;
-        //manager.onGameOver += ;
+        inputActions = new PlayerInputActions();
     }
 
     private void OnEnable()
     {
-        inputAction.Player.Enable();
-        inputAction.Player.LeftClick.performed += OnLeftPress;  //왼쪽 버튼 눌렀을때
-        inputAction.Player.LeftClick.canceled += OnLeftRelease; //왼 쪽 버 튼 떼 ㅆ을 때
-        inputAction.Player.RightClick.performed += OnRightClick;    //우측 마우스 버튼 클릭했을떄
-        inputAction.Player.MouseMove.performed += OnMouseMove;      //마우스의 움직임 
+        inputActions.Player.Enable();
+        inputActions.Player.LeftClick.performed += OnLeftPress;     // 왼버튼 누를 때
+        inputActions.Player.LeftClick.canceled += OnLeftRelease;    // 왼버튼 땠을 때
+        inputActions.Player.RightClick.performed += OnRightClick;   // 오른버튼 클릭
+        inputActions.Player.MouseMove.performed += OnMouseMove;     // 마우스 움직임
     }
+
     private void OnDisable()
     {
-        inputAction.Player.MouseMove.performed -= OnMouseMove;
-        inputAction.Player.RightClick.performed -= OnRightClick;
-        inputAction.Player.LeftClick.canceled -= OnLeftRelease;
-        inputAction.Player.LeftClick.performed -= OnLeftPress;
-        inputAction.Player.Disable();
+        inputActions.Player.MouseMove.performed -= OnMouseMove;
+        inputActions.Player.RightClick.performed -= OnRightClick;
+        inputActions.Player.LeftClick.canceled -= OnLeftRelease;
+        inputActions.Player.LeftClick.performed -= OnLeftPress;
+        inputActions.Player.Disable();
     }
 
     /// <summary>
     /// 이 보드가 가질 모든 셀을 생성하고 배치하는 함수
     /// </summary>
-    /// <param name="newWidth">보드의 가로길이</param>
-    /// <param name="newHeight">보드의 세로길이</param>
-    /// <param name="newMineCount">설치될 지뢰의 개수</param>
-    public void Initialize(int newWidth, int newHeight, int newMineCount)
+    /// <param name="newWidth">보드의 가로 길이</param>
+    /// <param name="newHieght">보드의 세로 길이</param>
+    /// <param name="newMineCount">배치될 지뢰의 개수</param>
+    public void Initialize(int newWidth, int newHieght, int newMineCount)
     {
+        // 값 설정
         width = newWidth;
-        height = newHeight;
+        height = newHieght;
         mineCount = newMineCount;
 
-        //셀 배열 만들기
+        // 셀 배열 만들기
         cells = new Cell[width * height];
 
+        // 셀 하나씩 생성 후 배열에 추가
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
                 GameObject cellObj = Instantiate(cellPrefab, transform);
                 Cell cell = cellObj.GetComponent<Cell>();
-                cell.transform.localPosition = new Vector3(x * Distance, -y * Distance);
 
                 int id = x + y * width;
                 cell.ID = id;
-                cells[id] = cell;
+                cell.transform.localPosition = new Vector3(x * Distance, -y * Distance);
+                cell.Board = this;
+
                 cellObj.name = $"Cell_{id}_({x},{y})";
+
+                cells[id] = cell;
             }
         }
-        ResetBoard();
-        MineCreate();
-    }
 
-    private void MineCreate()
-    {
-        for(int x = 0; x < 10;x++)
+        // 셀 전체 초기화
+        foreach (Cell cell in cells)
         {
-            cells[x].SetMine();
+            cell.Initialize();
         }
+
+        // 보드 데이터 리셋
+        ResetBoard();
     }
 
-    private void ResetBoard()
+    /// <summary>
+    /// 보드에 존재하는 모들 셀의 데이터를 리셋하고 지뢰를 새로 배치하는 함수(게임 재시작용 함수)
+    /// </summary>
+    void ResetBoard()
     {
+        // 전체 셀의 데이터 리셋
         foreach (Cell cell in cells)
         {
             cell.ResetData();
         }
 
-
+        // mineCount만큼 지뢰 배치하기
+        Shuffle(cells.Length, out int[] shuffleResult); // 숫자 섞기(0 ~ cells.length-1)
+        for (int i = 0; i < mineCount; i++)
+        {
+            cells[shuffleResult[i]].SetMine();
+        }
     }
 
+    // 셀 확인용 함수들 ------------------------------------------------------------------------------------
+
     /// <summary>
-    /// 스크린좌표를 그리드좌표로 볌ㄴ환하는 함수
+    /// 스크린 좌표를 그리드 좌표로 변경해주는 함수
     /// </summary>
-    /// <param name="screen">스크린ㄷ좌표</param>
-    /// <returns>변환된 그리드좌표</returns>
+    /// <param name="screen">스크린 좌표</param>
+    /// <returns>변환된 그리드 좌표</returns>
     Vector2Int ScreenToGrid(Vector2 screen)
     {
         Vector2 world = Camera.main.ScreenToWorldPoint(screen);
         Vector2 diff = world - (Vector2)transform.position;
 
-        return new Vector2Int(Mathf.FloorToInt( diff.x / Distance), Mathf.FloorToInt(-diff.y / Distance));
+        return new Vector2Int(Mathf.FloorToInt(diff.x / Distance), Mathf.FloorToInt(-diff.y / Distance));
     }
 
     /// <summary>
-    /// 그리드좌표를 인댁스로 변환 하는 함수
+    /// 그리드 좌표를 인덱스로 변경해주는 함수
     /// </summary>
-    /// <param name="x">x좌표</param>
-    /// <param name="y">y좌표</param>
-    /// <returns></returns>
+    /// <param name="x">x위치</param>
+    /// <param name="y">y위치</param>
+    /// <returns>변환된 인덱스 값(잘못된 그리드 좌표면 null)</returns>
     int? GridToIndex(int x, int y)
     {
         int? result = null;
-        if(IsValidGrid(x,y))
+        if (IsValidGrid(x, y))
         {
             result = x + y * height;
         }
@@ -150,27 +167,37 @@ public class Board : MonoBehaviour
     }
 
     /// <summary>
-    /// 지정된 그리드 좌표가 보드 내부인지 확인하느 ㄴ함수
+    /// 인덱스 값을 그리드 좌표로 변경해주는 함수
     /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns>보드 안이라면 true</returns>
-    bool IsValidGrid(int x, int y) 
-    { 
-        return x >= 0 && y >= 0 && x < width && y < height;
+    /// <param name="index">변경할 인덱스</param>
+    /// <returns>변경 완료된 그리드 좌표</returns>
+    Vector2Int IndexToGrid(int index)
+    {
+        return new(index % width, index / width);
     }
 
     /// <summary>
-    /// 특정 스크린 좌표에 ㅣㅇ?ㅆㄴ,ㄴ 셀을 리턴하는 함수
+    /// 지정된 그리드 좌표가 보드 내부인지 확인하는 함수
     /// </summary>
-    /// <param name="screen"></param>
-    /// <returns></returns>
+    /// <param name="x">x좌표</param>
+    /// <param name="y">y좌표</param>
+    /// <returns>보드 안이면 true, 밖이면 false, 셀이 초기화 되지 않아도 false</returns>
+    bool IsValidGrid(int x, int y)
+    {
+        return x >= 0 && y >= 0 && x < width && y < height && cells != null;
+    }
+
+    /// <summary>
+    /// 특정 스크린 좌표에 있는 셀을 리턴하는 함수
+    /// </summary>
+    /// <param name="screen">스크린 좌표</param>
+    /// <returns>셀이 없으면 null, 그 외에는 스크린 좌표에 있는 셀</returns>
     Cell GetCell(Vector2 screen)
     {
         Cell result = null;
         Vector2Int grid = ScreenToGrid(screen);
         int? index = GridToIndex(grid.x, grid.y);
-        if(index != null)
+        if (index != null)
         {
             result = cells[index.Value];
         }
@@ -178,38 +205,84 @@ public class Board : MonoBehaviour
         return result;
     }
 
-    #region InputActions 함수 ---------------------------------------------------------
-
-    // 입력에 따라 셀 변환
+    // 입력 처리용 함수들 ---------------------------------------------------------------------------------
     private void OnLeftPress(InputAction.CallbackContext context)
     {
         Vector2 screen = Mouse.current.position.ReadValue();
-        Cell testcell = GetCell(screen);
-        if(testcell == null)
-        {
-            Debug.Log("ㅇ벗어");
-        }
-        else
-        {
-            Debug.Log($"{testcell.gameObject.name}");
-        }
-
+        Debug.Log(GetCell(screen)?.gameObject.name);
     }
+
     private void OnLeftRelease(InputAction.CallbackContext context)
     {
-        Vector2 scress = Mouse.current.position.ReadValue();
+        Vector2 screen = Mouse.current.position.ReadValue();
     }
 
     private void OnRightClick(InputAction.CallbackContext context)
     {
-        Vector2 scress = Mouse.current.position.ReadValue();
+        Vector2 screen = Mouse.current.position.ReadValue();
+
+        // 이 위치에 있는 셀의 CellRightPress()가 실행된다.
     }
+
     private void OnMouseMove(InputAction.CallbackContext context)
     {
-        Vector2 scress = Mouse.current.position.ReadValue();
+        Vector2 screen = context.ReadValue<Vector2>();
     }
-    #endregion
 
+    // 기타 유틸리티 함수들 ------------------------------------------------------------------------------
+
+    /// <summary>
+    /// 셔플용 함수
+    /// </summary>
+    /// <param name="count">셔플할 숫자 범위(0 ~ count-1)</param>
+    /// <param name="result">셔플된 결과</param>
+    void Shuffle(int count, out int[] result)
+    {
+        // count만큼 순서대로 숫자가 들어간 배열 만들기
+        result = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = i;
+        }
+
+        // 위에서 만든 배열을 섞기
+        int loopCount = result.Length - 1;
+        for (int i = 0; i < loopCount; i++) // 8*8일 때 63번 반복
+        {
+            int randomIndex = UnityEngine.Random.Range(0, result.Length - i); // 처음에는 0~63 중 랜덤으로 선택
+            int lastIndex = loopCount - i;                                      // 처음에는 63
+
+            // 랜덤으로 고른 것과 마지막을 스왑
+            (result[lastIndex], result[randomIndex]) = (result[randomIndex], result[lastIndex]);
+        }
+    }
+
+    /// <summary>
+    /// 특정 셀의 주변 셀을 돌려주는 함수
+    /// </summary>
+    /// <param name="id">중심 셀의 아이디</param>
+    /// <returns>중심 셀의 주변 셀들의 리스트</returns>
+    public List<Cell> GetNeightbors(int id)
+    {
+        List<Cell> result = new List<Cell>();
+        Vector2Int grid = IndexToGrid(id);    // id의 그리드 위치를 x와 y 모두 +-1씩해서 구하기
+        for (int y = -1; y < 2; y++)
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                if (!(x == 0 && y == 0))         // 자기 자신은 제외
+                {
+                    int? index = GridToIndex(x + grid.x, y + grid.y);
+                    if (index != null)           // valid한 id면 추가
+                    {
+                        result.Add(cells[index.Value]);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 
 #if UNITY_EDITOR
     public void Test_OpenAllCover()
@@ -218,6 +291,11 @@ public class Board : MonoBehaviour
         {
             cell.Test_OpenCover();
         }
+    }
+
+    public void Test_BoardReset()
+    {
+        ResetBoard();
     }
 #endif
 }
--- Assets/Scripts/Board/Cell.cs
+++ Assets/Scripts/Board/Cell.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    /// 보드 입력에 따른 cover이미지 변경
-    /// 열기 / 닫기 
-    
-    
-    int? id = null; 
+    // 열기/닫기
+    // 보드에 입력에 따른 cover 이미지 변경
+
+    /// <summary>
+    /// 이 셀의 ID(위치계산에도 사용될 수 있음)
+    /// </summary>
+    int? id = null;
 
     public int ID
     {
-        get => id.GetValueOrDefault(); // 0일 경우 맞을 수도 있고 아닐 수도 있다.
+        get => id.GetValueOrDefault();  // 0일 경우 맞을 수도 있고 아닐수도 있다.
         set
         {
-            if (id == null)     //이 프로퍼티는 한 번만 설정 가능하다.
+            if (id == null)    // 이 프로퍼티는 한번만 설정 가능하다.
             {
                 id = value;
             }
@@ -23,59 +26,119 @@ public class Cell : MonoBehaviour
     }
 
     /// <summary>
-    /// 겉면의 스프라이트 렌더러
+    /// 겉면의 스프라이트 랜더러(Close, Question, Flag)
     /// </summary>
-    [SerializeField] SpriteRenderer cover;
+    SpriteRenderer cover;
 
     /// <summary>
-    /// 안쪽의 스프라이트 렌더러
+    /// 안쪽의 스프라이트 랜더러(지뢰, 주변 지뢰 개수)
     /// </summary>
-    [SerializeField] SpriteRenderer inside;
+    SpriteRenderer inside;
 
+    /// <summary>
+    /// 셀에 지뢰가 있는지 여부
+    /// </summary>
     bool hasMine = false;
+
+    /// <summary>
+    /// 지뢰 설치 여부를 확인하기 위한 프로퍼티
+    /// </summary>
     public bool HasMine => hasMine;
 
+    /// <summary>
+    /// 이 셀이 관리되는 보드
+    /// </summary>
     Board parentBoard = null;
     public Board Board
     {
         get => parentBoard;
         set
         {
-            if(parentBoard == null) 
+            if (parentBoard == null)    // 한번만 설정 가능
+            {
                 parentBoard = value;
+            }
         }
     }
 
+    /// <summary>
+    /// 자기 주변 셀의 목록
+    /// </summary>
+    List<Cell> neighbors;
+
+    /// <summary>
+    /// 자기 주변의 지뢰 개수
+    /// </summary>
+    int aroundMineCount = 0;
+
+    /// <summary>
+    /// 셀이 열렸는지 여부
+    /// </summary>
+    bool isOpen = false;
 
     private void Awake()
     {
         Transform child = transform.GetChild(0);
         cover = child.GetComponent<SpriteRenderer>();
-
         child = transform.GetChild(1);
         inside = child.GetComponent<SpriteRenderer>();
-
-
     }
 
+    /// <summary>
+    /// 셀 생성 초기화 함수(처음에 단 한번만 실행되면 됨)
+    /// </summary>
+    public void Initialize()
+    {
+        neighbors = Board.GetNeightbors(ID);    // 이웃 셀 저장해 놓기
+    }
+
+    /// <summary>
+    /// 이 셀의 데이터를 초기화 하는 함수
+    /// </summary>
     public void ResetData()
     {
-        Board = FindAnyObjectByType<Board>();
         hasMine = false;
+        aroundMineCount = 0;
+        isOpen = false;
+
         cover.sprite = Board[CloseCellType.Close];
         inside.sprite = Board[OpenCellType.Empty];
         cover.gameObject.SetActive(true);
     }
 
     /// <summary>
-    /// 지뢰 배치 여부에 따라 inside 이미지 변경
+    /// 이 셀에 지뢰를 설치하는 함수
     /// </summary>
     public void SetMine()
     {
-        hasMine = true;
-        inside.sprite = Board[OpenCellType.Mine];
+        hasMine = true;                             // 지뢰 설치했다고 표시
+        inside.sprite = Board[OpenCellType.Mine];   // 이미지 변경
+
+        foreach (Cell cell in neighbors)
+        {
+            cell.IncreaseAroundMineCount(); // 주변 셀의 주변 지뢰 개수 증가
+        }
     }
 
+    /// <summary>
+    /// 주변 지뢰 개수 증가용 함수
+    /// </summary>
+    void IncreaseAroundMineCount()
+    {
+        if (!hasMine)   // 지뢰가 아닐 때만 개수 증가
+        {
+            aroundMineCount++;                                      // 주변 지뢰 개수 증가
+            inside.sprite = Board[(OpenCellType)aroundMineCount];   // 주변 지뢰 개수에 맞게 이미지 변경
+        }
+    }
+
+    /// <summary>
+    /// 셀이 우클릭되면 실행되는 함수
+    /// </summary>
+    public void CellRightPress()
+    {
+
+    }
 
 #if UNITY_EDITOR
     public void Test_OpenCover()
@@ -83,4 +146,5 @@ public class Cell : MonoBehaviour
         cover.gameObject.SetActive(false);
     }
 #endif
+
 }
